c8cb47fb07b441f27ec05cce0fc9342ab6ca6ef3
Zhang <heartheros@163.com>
2018-11-29 09:23:30 +0800

    add table mappers and xml

--- src/main/resources/_schema.sql
+++ src/main/resources/_schema.sql
@@ -64,9 +64,9 @@ CREATE TABLE `T_VERIFICATION_TOKEN` (
 #add for recuritment test project
 use db_springboot_forum
 
-drop table r_user if exists;
+drop table IF EXISTS r_user;
 create table r_user(
-id 	                 varchar(30) not null comment '用户id',
+id 	                int not null auto_increment comment '用户id',
 nickname	           varchar(30) not null comment '用户昵称',
 name	               varchar(30) comment '用户姓名',
 crdt_type	           char(2) comment '证件类型',
@@ -77,14 +77,15 @@ email	               varchar(80) comment '邮箱地址',
 busi_type_user	     char(2)  not null default '00' comment '网站归类 ',
 remark1	             varchar(80) comment '个人标签1',
 remark2	             varchar(80) comment '个人标签2',
-remark3	             varchar(80) comment '个人标签3'
+remark3	             varchar(80) comment '个人标签3',
+ primary key(id)
 );
-alter table r_user add primary key(id);
 alter table r_user add unique(nickname);
-drop table r_company if exists;
+
+drop table if exists r_company ;
 
 create table r_company(
-id 	                 varchar(30) not null comment '公司id',
+id 	                 int not null auto_increment comment '公司id',
 nickname	           varchar(30) not null comment '公司昵称',
 name	               varchar(80) comment '公司名',
 crdt_code	           varchar(30)  comment '单位信用代码',
@@ -97,16 +98,16 @@ field               char(2) comment '公司行业性质',
 size                char(2) comment '公司规模人数',
 regicapital        int comment '注册资金万元',
 type                 char(4) not null default '0006' comment '单位类型',
-busi_type_comp	     char(2)  not null default '00' comment '网站归类 '
+busi_type_comp	     char(2)  not null default '00' comment '网站归类 ',
+primary key(id)
 );
-alter table r_company add primary key(id);
 alter table r_company add unique(nickname);
 
-drop table r_job if exists;
+drop table if exists r_job ;
 
 create table r_job(
-id 	                 varchar(30) not null comment '职位id',
-comp	           varchar(30) not null comment '发布公司id',
+id            int not null auto_increment comment '职位id',
+comp	           int not null comment '发布公司id',
 post_date	               date comment '发布日期',
 expiry_date	           date  comment '到期日期',
 title	           varchar(60)  comment '职位名称',
@@ -115,26 +116,26 @@ abstract	           varchar(200)  comment '职位摘要',
 content	             varchar(4096) comment '职位描述',
 phone_num	               varchar(50) comment '电话',
 email                 varchar(80) not null default '0006' comment '邮箱地址',
-busi_type_job	     char(2)  not null default '00' comment '网站归类 '
+busi_type_job	     char(2)  not null default '00' comment '网站归类 ',
+primary key(id)
 );
-alter table r_job add primary key(id);
 alter table r_job add index COMPANY_INDEX(comp);
 
-drop table r_apply if exists;
+drop table if exists r_apply ;
 
 create table r_apply(
-userid varchar(30) not null comment '用户id',
-jobid  varchar(30) not null comment '职位id',
+userid int not null comment '用户id',
+jobid  int not null comment '职位id',
 apply_date date not null comment '申请日期',
 apply_status char(1) not null default '0' comment '申请状态',
-compId  varchar(30) not null comment '公司id'
+compId  int not null comment '公司id'
 );
 alter table r_apply add primary key(userid,jobid,apply_date);
 alter table r_apply add index COMPANY_INDEX(compId);
 
-drop table r_post if exists;
+drop table if exists r_post ;
 create table r_post(
-id	          varchar(30) not null comment '文章ID',
+id int not null auto_increment comment '文章id',
 category      char(2) not null default '00' comment '文章类别',
 title	      varchar(90) not null comment '标题',
 subtitle	  varchar(90) not null comment '副标题',
@@ -148,18 +149,18 @@ reviewer	    varchar(30) not null comment '审核者昵称',
 postsource	      varchar(80) not null comment  '来源',
 clickcount	  int comment  '点击次数',
 collectcount int comment '收藏次数',
-comments	    char(1) not null comment '用可评论标识'
+comments	    char(1) not null comment '用可评论标识',
+primary key(id)
 );
-alter table r_post add primary key(id);
 alter table r_post add index CATEGORY_INDEX(category);
 alter table r_post add index DATE_INDEX(postdate);
 
-drop table r_comment if exists;
+drop table if exists r_comment ;
 create table r_comment(
 id int not null auto_increment comment '评论ID',
 content		varchar(4096) comment 'html内容',
-postid		varchar(30) comment '文章id',
-userid		varchar(30) comment '用户id',
+postid		int not null comment '文章id',
+userid		int not null comment '用户id',
 commentsn	int comment '评论序号',
 createdate date comment '创建日期',
 createtime timestamp(3) default current_timestamp(3) comment '发布时间',
@@ -168,20 +169,22 @@ PRIMARY KEY (`id`)
 alter table r_comment add index userid_INDEX(userid);
 alter table r_comment add index postid_INDEX(postid);
 
-drop table r_token if exists;
+drop table if exists r_token ;
 create table r_token(
 id int not null auto_increment comment '令牌序号',
 token varchar(512) comment '令牌',
-userid varchar(30) not null comment '用户id',
+userid int not null comment '用户id',
 expirydate date default '2099-12-30' comment '令牌到期日',
 PRIMARY KEY (`id`)
 );
 
-drop table r_param if exists;
+drop table if exists r_param ;
 create table r_param(
 id int not null auto_increment comment '参数id',
 param_name varchar(30) comment '参数名称',
-param_value varchar(80) comment '参数值'
+param_value varchar(80) comment '参数值',
+PRIMARY KEY (`id`)
 )
-
+alter table r_param add unique(param_name);
+commit;
 
